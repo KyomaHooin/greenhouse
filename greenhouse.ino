b6325f2125015caa899db520d7a307c3e99eb082
KyomaHooin <richard_bruna@nm.cz>
2016-09-18 17:08:19 +0200

    Touch, TFT code.

--- greenhouse.ino
+++ greenhouse.ino
@@ -2,38 +2,91 @@
 // 2.4 TFT LCD touch "Hothouse" light control 
 //
 
-//CONST
-
-const int CH[3] = {A0,A1,A3};      // N-MOSFET channels signal pin
-const int T[3] = {6,7,8};          // touch sensor signal pin
-const int PRODLEVA_TOUCH = 15000;  // 15s touch press delay
+#include <Adafruit_GFX.h>
+#include <MCUFRIEND_kbv.h>
+#include "TouchScreen.h"
 
 //VAR
 
-long touchTime[3] = {0,0,0};       // Timer
+//TFT instance
+MCUFRIEND_kbv tft;
+//Touch instance
+TouchScreen ts = TouchScreen(9, A2, A3, 8, 300);
+//CONST/VAR
+const uint16_t ID = 0x9325;// TFT ID
+//const int CH[3] = {A0,A1,A2};      // IRF540N signal pin
+const int CH = A5;                   // IRF540N signal pin
+//const int T[3] = {A3,A4,A5};       // TP223 signal pin
+const int T = 10;                    // TP223 signal pin
+//Delay
+const int DELAY_TOUCH = 15000;    // 15s touch press
+const int DELAY_SCREEN = 100;     // 10ms screen press
+//Timer
+long touchTime, screenTime, fadeTime[3];
 
 //SETUP
 
 void setup() {
-  //Debug
+  //DEBUG
   Serial.begin(9600);
-  //Touch, FET setup
-  for (int i = 0; i < 3; i++) {
-    pinMode(T[i],INPUT);
-    pinMode(CH[i],OUTPUT);
-    digitalWrite(CH[i],LOW);
-  }
+  //TOUCH/FET
+  //for (int i = 0; i < 3; i++) {
+  //  pinMode(T[i],INPUT);
+    pinMode(T,INPUT);
+  //  pinMode(CH[i],OUTPUT);
+  pinMode(CH,OUTPUT);
+  //  digitalWrite(CH[i],LOW);
+  digitalWrite(CH,LOW);
+  //}
+  //TFT
+  tft.reset();
+  tft.begin(ID);
+  tft.setRotation(1);// LANDSCAPE
+  tft.fillScreen(0x0000);// BLACK
+  tft.setCursor(110, 110);
+  tft.setTextSize(2);
+  tft.println("DEMO CODE");
 }
 
 //MAIN
 
 void loop() {
-  //touch solving
-  for (int i = 0; i < 3; i++) {
-    if ( millis() - touchTime[i] > PRODLEVA_TOUCH && digitalRead(T[i]) == HIGH ) {
-       digitalRead(CH[i]) ? digitalWrite(CH[i],LOW) : digitalWrite(CH[i],HIGH);
-       touchTime[i] = millis();
+  //TOUCH
+  //for (int i = 0; i < 3; i++) {
+    //if ( millis() - touchTime[i] > DELAY_TOUCH && digitalRead(T[i]) == HIGH ) {
+    if ( millis() - touchTime > DELAY_TOUCH && digitalRead(T) == HIGH ) {        
+      //digitalRead(CH[i]) ? digitalWrite(CH[i],LOW) : digitalWrite(CH[i],HIGH);
+      digitalRead(CH) ? digitalWrite(CH,LOW) : digitalWrite(CH,HIGH);
+      //reset timer
+      //touchTime[i] = millis();
+      touchTime = millis();
     }
+  //}
+  //SCREEN
+  if ( millis() - screenTime > DELAY_SCREEN ) {
+    //ScreenTouch coord.
+    TSPoint p = ts.getPoint(); //p.x p.y //p.z
+    if (p.z > ts.pressureThreshhold) {
+      Serial.print("x: "); Serial.print(p.x);
+      Serial.print(" y: "); Serial.print(p.y);
+      Serial.print(" z: "); Serial.println(p.z);
+    }
+    //reset timer
+    screenTime = millis();
   }
+  //softPWM
+  //for (int f = 1; f < 254; f++) {
+  //  softPWM(CH[0], f , 20);// freq, speed
+  //}
+  //for (int f = 254; f >  1; f--) {
+  //  softPWM(CH[0], f, 20);// freq, speed
+  //}
 }
+
+//void softPWM(int pin, int freq, int spd) {
+//  digitalWrite(pin,HIGH);
+//  delayMicroseconds(spd*freq);
+//  digitalWrite(pin,LOW);
+//  delayMicroseconds(spd*(255-freq));
+//}
 
