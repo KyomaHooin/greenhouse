3c63db14dcdfdf442d1891bbb0aa1e05c4b27f81
KyomaHooin <richard_bruna@nm.cz>
2016-09-22 20:43:55 +0200

    Fade touch, screensaver update.

--- greenhouse.ino
+++ greenhouse.ino
@@ -1,16 +1,12 @@
 
 // 2.4 TFT LCD touch "Hothouse" light control 
 //
-// TODO:
-//
-// double touch ?
-// bitmap ?
 
-#include <Adafruit_GFX.h>
+#include </root/avr/greenhouse/openhard.h>
 #include <MCUFRIEND_kbv.h>
+#include <Adafruit_GFX.h>
 #include "TouchScreen.h"
 #include <EEPROM.h>
-#include </root/avr/greenhouse/openhard.h>
 
 #define BLACK   0x0000
 #define WHITE   0xFFFF
@@ -31,22 +27,17 @@ TSPoint tp;
 
 //CONST/VAR
 
-const uint16_t ID = 0x9325;       // TFT ID
+const int T[2] = {10};// TP223 signal pin
+const int CH[2] = {A5};// IRF540N signal pin
 
-const int T[2] = {10,11};         // TP223 signal pin
-const int CH[2] = {A5};           // IRF540N signal pin
+const unsigned int DELAY_NOTOUCH = 60000;// 1m no TFT touch
+const int DELAY_SENSOR = 15000;// 15s sensor touch press
+const int DELAY_PRESS = 100;// 100ms general screen press
+const int DELAY_MENU = 1000;// 1s menu press
 
-const int DELAY_NOTOUCH = 30000;  // 30s  no TFT touch
-const int DELAY_SENSOR = 15000;   // 15s sensor touch press
-const int DELAY_PRESS = 100;      // 100ms screen press
-const int DELAY_MENU = 1000;      // 500ms screen press
-
-long touchTime[2], fadeTime[2], menuTime, screenTime; //timer
-
-bool TMODE[2];                    //TP223 on/off status
-bool CHMODE[2];                   //channel select status
-bool PWMODE[2];                   //channel PWM status
-bool Token = 1;                   //screen level token
+long touchTime[2], fadeTime[2], menuTime, screenTime;// timer
+bool TMODE[2], CHMODE[2], PWMODE[2];// TP223 on/off, channel, PWM status
+bool Token = 1;// screen level token
 
 //SETUP
 
@@ -59,28 +50,28 @@ void setup() {
     pinMode(CH[i],OUTPUT);
     digitalWrite(CH[i],LOW);
     //TMODE[i] = EEPROM.read(i);
+    //PWMODE[i] = EEPROM.read(i + 2);
   }
   //TFT
   tft.reset();
-  tft.begin(ID);
-  tft.setRotation(1);//landscape
+  tft.begin(0x9325);// Driver ID
+  tft.setRotation(1);// "landscape"
   drawLogo();
 }
 
 //MAIN
 
 void loop() {
-  //TOUCH
+  //SENSOR
   for (int i = 0; i < 2; i++) {
     if ( millis() - touchTime[i] > DELAY_SENSOR && digitalRead(T[i]) == HIGH ) {
-      if (digitalRead(CH[i])) { digitalWrite(CH[i],LOW); TMODE[i] = 0; } else { digitalWrite(CH[i],HIGH); TMODE[i] = 255; }
+      if (digitalRead(CH[i])) { digitalWrite(CH[i],LOW); TMODE[i] = 0; } else { digitalWrite(CH[i],HIGH); TMODE[i] = 1; }
       //Update menu
       if (Token == 0 ) { fixPin(); colorOnoff(); }
       //reset timer
       touchTime[i] = millis();
     }
   }
-
   //SCREEN
   if ( millis() - screenTime > DELAY_PRESS ) {
     //ScreenTouch coord.
@@ -98,27 +89,31 @@ void loop() {
         CHMODE[1] = 1; CHMODE[0] = 0; fixPin(); colorChann();
       }
     }
-    //ONOFF TOUCH
-    //FADER TOUCH
-    //RESET
-    if (tp.z > MINPRESS && tp.z < MAXPRESS) {// catch sharing pin..
-      Serial.print("x: "); Serial.print(tp.x);
-      Serial.print(" y: "); Serial.print(tp.y);
-      Serial.print(" z: "); Serial.println(tp.z);
-      
-      //reset timer
-      screenTime = millis();
+    //ON-OFF TOUCH
+    if (715 < tp.x && tp.x < 770 && 560 < tp.y && tp.y < 645 && tp.z > MINPRESS && tp.z < MAXPRESS && !Token) {// CH0 ON
+      if (CHMODE[0] == 1 && TMODE[0] == 0) { digitalWrite(CH[0],HIGH); TMODE[0] = 1; fixPin(); colorOnoff(); }
     }
+    if (595 < tp.x && tp.x < 650 && 560 < tp.y && tp.y < 645 && tp.z > MINPRESS && tp.z < MAXPRESS && !Token) {// CH0 OFF
+      if (CHMODE[0] == 1 && TMODE[0] == 1) { digitalWrite(CH[0],LOW); TMODE[0] = 0; fixPin(); colorOnoff(); }
+    }
+    if (465 < tp.x && tp.x < 530 && 560 < tp.y && tp.y < 645 && tp.z > MINPRESS && tp.z < MAXPRESS && !Token) {// CH1 ON
+      if (CHMODE[1] == 1 && TMODE[1] == 0) { digitalWrite(CH[1],HIGH); TMODE[1] = 1; fixPin(); colorOnoff(); }
+    }
+    if (345 < tp.x && tp.x < 410 && 560 < tp.y && tp.y < 645 && tp.z > MINPRESS && tp.z < MAXPRESS && !Token) {// CH1 OFF
+      if (CHMODE[1] == 1 && TMODE[1] == 1) { digitalWrite(CH[1],LOW); TMODE[1] = 0; fixPin(); colorOnoff(); }
+    }
+    //FADER TOUCH
+    if (710 < tp.x && tp.x < 780 && 725 < tp.y && tp.y < 810 && tp.z > MINPRESS && tp.z < MAXPRESS && !Token) {// FADE-
+      if ((CHMODE[0] == 1 && TMODE[0] == 1) || (CHMODE[1] == 1 && TMODE[1] == 1)) { Serial.println("Fade-!"); }
+    }
+    if (345 < tp.x && tp.x < 405 && 725 < tp.y && tp.y < 810 && tp.z > MINPRESS && tp.z < MAXPRESS && !Token) {// FADE+
+      if ((CHMODE[0] == 1 && TMODE[0] == 1)||(CHMODE[1] == 1 && TMODE[1] == 1)) { Serial.println("Fade+!"); }
+    }
+    //RESET
+    if (tp.z > MINPRESS && tp.z < MAXPRESS) { screenTime = millis(); }
   }
   //SCREENSAVER
-  if ( millis() - screenTime > DELAY_NOTOUCH && Token == 0 ) { CHMODE[0]= 0; CHMODE[1] = 0; fixPin(); drawLogo(); Token = 1; }
-  //PWM
-  //for (int f = 1; f < 254; f++) {
-  //  softPWM(CH[0], f , 20);// freq, speed
-  //}
-  //for (int f = 254; f >  1; f--) {
-  //  softPWM(CH[0], f, 20);// freq, speed
-  //}
+  if (millis() - screenTime > DELAY_NOTOUCH && Token == 0) { CHMODE[0]= 0; CHMODE[1] = 0; fixPin(); drawLogo(); Token = 1; }
 }
 
 //FUNC
@@ -128,21 +123,28 @@ void drawMenu() {
   tft.fillScreen(BLACK);
   colorChann();
   colorOnoff();
-  drawFaderBar();
   drawFader();
 }
-
-void drawFaderBar() {
- tft.drawRect(65,180,190,15, WHITE);
- tft.drawRect(66,181,188,13, WHITE);
-}
-
+//Fader
 void drawFader() {
- tft.drawRect(75,175,35,25, WHITE);
- tft.drawRect(76,176,33,23, WHITE);
- tft.fillRect(77,177,31,21, BLACK);
+ tft.drawRect(65,170,40,40, WHITE);// [-]
+ tft.drawRect(66,171,38,38, WHITE);
+ tft.drawRect(215,170,40,40, WHITE);// [+]
+ tft.drawRect(216,171,38,38, WHITE);
+ tft.setCursor(80, 183);
+ tft.setTextSize(2);
+ tft.println("-");
+ tft.setCursor(230, 183);
+ tft.setTextSize(2);
+ tft.println("+");
+ tft.drawRect(115,190,90,2, WHITE);// line
+ tft.drawRect(115,186,2,10, WHITE);// 1st
+ tft.drawRect(137,186,2,10, WHITE);// 2nd
+ tft.drawRect(159,186,2,10, WHITE);// 3rd
+ tft.drawRect(181,186,2,10, WHITE);// 4th
+ tft.drawRect(204,186,2,10, WHITE);// 5th
 }
-
+//90x90 square + 10px spacing
 void colorChann() {
   for (int i = 0; i < 2; i++) {
     if (CHMODE[i]) {
@@ -158,7 +160,7 @@ void colorChann() {
     tft.println(i);   
   }
 }
-
+//40x40 square + 10px spacing
 void colorOnoff() {
   for (int i = 0; i < 2; i++) {
     if (TMODE[i]) {
@@ -180,7 +182,7 @@ void colorOnoff() {
     tft.println("OFF");
   }
 }
-
+//Bitmap PROGMEM logo
 void drawLogo() {
   tft.fillScreen(BLACK);
   //draw PROGMEM 16bit color img pixel by pixel..
@@ -195,7 +197,6 @@ void drawLogo() {
   tft.setTextSize(1);
   tft.println("GREENHOUSE SYSTEM Ltd.");
 }
-
 //TFT/Touch sharing pins reset
 void fixPin() {
   pinMode(A2, OUTPUT);
@@ -203,7 +204,15 @@ void fixPin() {
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
 }
-
+//PWM fading..
+void pwm() {
+ for (int f = 1; f < 254; f++) {
+   softPWM(CH[0], f , 20);// freq, speed
+  }
+  for (int f = 254; f >  1; f--) {
+    softPWM(CH[0], f, 20);// freq, speed
+  }
+}
 //Software PWM wave
 void softPWM(int pin, int freq, int spd) {
   digitalWrite(pin,HIGH);
